08e7d88d165276edd84eec24587c3d8d5ee0acb2
alemalagni <alemalagni@gmail.com>
2025-03-15 18:14:56 +0100

    feat: add API fecth

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Script
+
+for ( let i = 0; i < 6; i++ ) {
+    fetch("https://lanciweb.github.io/demo/api/pictures/")
+    .then( response => response.json() )
+    .then( data => {
+        
+    });
+}
