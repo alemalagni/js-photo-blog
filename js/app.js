46e8f6044fbd3573dedc9af48c393b5b5c4e8430
alemalagni <alemalagni@gmail.com>
2025-03-15 18:36:48 +0100

    feat: add API link

--- js/app.js
+++ js/app.js
@@ -5,12 +5,12 @@ for ( let i = 0; i < 6; i++ ) {
     fetch("https://lanciweb.github.io/demo/api/pictures/")
     .then( response => response.json() )
     .then( data => {
-        
+
         photo.innerHTML = photo.innerHTML + `
-        <div class="cardPhoto"> <!-- Foto 1 --> 
+        <div class="cardPhoto"> 
                 <div class="pin"></div>
-                <div class="photo"><span>600 x 600</span></div>
-                <div class="caption"><span>Didascalia 1</span></div>
+                <div class="photo"><img url=${data[i]} alt=""></div>
+                <div class="caption"><span>${data[i].title}</span></div>
             </div>
         `;
     });
