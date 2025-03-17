38241ae28bc15410391692ef3a6642ff7c887d48
alemalagni <alemalagni@gmail.com>
2025-03-17 21:54:15 +0100

    feat: add <a> to photo script

--- js/app.js
+++ js/app.js
@@ -7,12 +7,14 @@ for ( let i = 0; i < 6; i++ ) {
     .then( data => {
 
         photo.innerHTML = photo.innerHTML + `
-        <div class="cardPhoto"> 
+        <div class="cardPhoto" id="${i+1}">
+            <a class="linkPhoto"> 
                 <div class="pin"></div>
                 <div class="photo"><img src="${data[i].url}" alt="" style="width: 260px; display: block;"></div>
                 <div class="caption"><span>${data[i].title}</span></div>
                 <div class="caption date"><span>${data[i].date}</span></div>
-            </div>
+            </a>
+        </div>
         `;
     });
 }
