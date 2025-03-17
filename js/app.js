3d114938d89ddd010c8d09b679eb2cdbaa76f90e
alemalagni <alemalagni@gmail.com>
2025-03-17 09:48:43 +0100

    fix: JS script

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ for ( let i = 0; i < 6; i++ ) {
         photo.innerHTML = photo.innerHTML + `
         <div class="cardPhoto"> 
                 <div class="pin"></div>
-                <div class="photo"><img url="${data[i].url}" alt="" style="width: 260px; padding-bottom:260px;"></div>
+                <div class="photo"><img src="${data[i].url}" alt="" style="width: 260px; padding-bottom:260px;"></div>
                 <div class="caption"><span>${data[i].title}</span></div>
                 <div class="caption date"><span>${data[i].date}</span></div>
             </div>
