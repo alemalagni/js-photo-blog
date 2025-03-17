4c5792bee833ad509dd6ddb902936f790aff375f
alemalagni <alemalagni@gmail.com>
2025-03-17 22:19:18 +0100

    fix: JS script

--- js/app.js
+++ js/app.js
@@ -1,20 +1,16 @@
 // Script
-const photo = document.getElementById("photoCollection");
+const photo = document.getElementsByClassName("cardPhoto");
 
 for ( let i = 0; i < 6; i++ ) {
     fetch("https://lanciweb.github.io/demo/api/pictures/")
     .then( response => response.json() )
     .then( data => {
 
-        photo.innerHTML = photo.innerHTML + `
-        <div>
-            <button class="cardPhoto"> 
+        photo[i].innerHTML = photo[i].innerHTML + `
                 <div class="pin"></div>
                 <div class="photo"><img src="${data[i].url}" alt="" style="width: 260px; display: block;"></div>
                 <div class="caption"><span>${data[i].title}</span></div>
                 <div class="caption date"><span>${data[i].date}</span></div>
-            </button>
-        </div>
         `;
     });
 }
